57268d1496839dbb70aea488897901174a558f6f
gitanjali-13 <gitaanjalide@gmail.com>
2020-05-16 16:28:34 +0530

    salary of employee

---
+++ empWageComp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+echo "Welcome msg"
+ispresent=1;
+randomCheck=$((RANDOM%2));
+if [ $ispresent -eq $randomCheck ]
+then
+      salaryperHour=20;
+      workingHourperday=8;
+      salary=$(($salaryperHour * $workingHourperday))
+else
+     salary=0;
+fi
+echo $salary;
